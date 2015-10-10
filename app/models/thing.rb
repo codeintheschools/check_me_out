a633c10fd955cba554d81cce11eb56e999470d36
Paul Tyng <paul@paultyng.net>
2015-10-10 01:47:09 +0000

    Add some additional validation on thing

--- app/models/thing.rb
+++ app/models/thing.rb
@@ -1,7 +1,8 @@
 class Thing < ActiveRecord::Base
   has_many :check_outs
 
-  validates :quantity, numericality: { greater_than: 0 }
+  validates :quantity, numericality: { greater_than_or_equal_to: 0 }
+  validates :name, presence: true
 
   def available?
     available_quantity > 0
