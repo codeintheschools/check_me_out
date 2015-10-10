5f81673e3e528b02628229c826820850302a8967
Paul Tyng <paul@paultyng.net>
2015-10-10 00:22:20 +0000

    Fix n+1 query issue on home page

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,6 +1,6 @@
 class WelcomeController < ApplicationController
   def index
-    @things = Thing.all.order(:name)
+    @things = Thing.all.includes(:check_outs).order(:name)
     respond_with @things
   end
 end
