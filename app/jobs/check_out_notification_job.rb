646475ed535e0436df99fe85a9b7cfcd6e8051db
Paul Tyng <paul@paultyng.net>
2015-10-12 15:50:32 +0000

    Clean up syntax in job

--- app/jobs/check_out_notification_job.rb
+++ app/jobs/check_out_notification_job.rb
@@ -4,11 +4,12 @@ class CheckOutNotificationJob < ActiveJob::Base
   def perform(check_out)
     slack_webhook_url = ENV['SLACK_WEBHOOK_URL']
 
-    unless slack_webhook_url.blank?
-      message = "#{check_out.name} #{check_out.pending? ? 'checked out' : 'returned'} #{check_out.quantity} #{check_out.thing.name} (#{check_out.thing.available_quantity} remaining)"
+    return if slack_webhook_url.blank?
 
-      notifier = Slack::Notifier.new(slack_webhook_url, username: 'Check Me Out!', icon_emoji: ':memo:')
-      notifier.ping message
-    end
+    msg = "#{check_out.name} #{check_out.pending? ? 'checked out' : 'returned'}"
+    msg += " #{check_out.quantity} #{check_out.thing.name} (#{check_out.thing.available_quantity} remaining)"
+
+    notifier = Slack::Notifier.new(slack_webhook_url, username: 'Check Me Out!', icon_emoji: ':memo:')
+    notifier.ping msg
   end
 end
